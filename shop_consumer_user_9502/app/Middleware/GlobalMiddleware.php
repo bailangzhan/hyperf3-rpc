90832885de94341ef065471aa0f079ebcfaf1d88
bailangzhan <bailangzhan@qq.com>
2022-10-10 17:12:20 +0800

    zipkin支持获取traceId

---
+++ shop_consumer_user_9502/app/Middleware/GlobalMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Hyperf\Context\Context;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class GlobalMiddleware implements MiddlewareInterface
+{
+    public function __construct(protected ContainerInterface $container)
+    {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // response 添加traceId
+        $span = Context::get('tracer.root');
+        $response = Context::get(ResponseInterface::class);
+        $response = $response->withHeader('Trace-Id', $span->getContext()->getContext()->getTraceId());
+        Context::set(ResponseInterface::class, $response);
+
+        return $handler->handle($request);
+    }
+}
